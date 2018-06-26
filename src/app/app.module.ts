2c96c26110bf328294eef51765e4159297d71acd
gonzaloan <gonzaloan.munoz@gmail.com>
2018-06-26 12:32:38 -0400

    agrega nav bar, estilos y componente RUN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { RunComponent } from './components/run/run.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    RunComponent
   ],
   imports: [
     BrowserModule
